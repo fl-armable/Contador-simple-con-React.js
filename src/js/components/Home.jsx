fb1c9e66f5f83c36216edde7f792baf9e9aa2d39
fl-armable <fl.irlanda@gmail.com>
2025-05-10 02:16:27 +0000

    Second commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,33 +4,68 @@ import React, { useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	const [isTimer, setIsTimer] = React.useState(false);
-	const [isPaused, setIsPaused] = React.useState(true);
-	const [toTimer, setToTimer] = React.useState(360);
-	const [displayTime, setDisplayTime] = React.useState(() => (isTimer ? toTimer : 0))
+	const [isTimer, setIsTimer] = React.useState(false); // Para seleccionar el temporizador
+	const [isPaused, setIsPaused] = React.useState(true); // Para seleccionar Inicio o Pausa
+	const [toTimer, setToTimer] = React.useState(360); // Guarda el valor del temporizador
+	const [displayTime, setDisplayTime] = React.useState(() => (isTimer ? toTimer : 0)) // Valor mostrado
 
 	useEffect(() => {
 		if (isPaused) {
 			return;
-		}
+		} // Detiene el cronómetro/temporizador
+		if (displayTime <= 0 && isTimer) {
+				setIsPaused(true);
+				alert("Time's up!");
+				return;
+			} // Detiene el temporizador cuando llega a 0 y nuestra una alerta
 		const interval = setInterval(() => {
 			if(!isTimer) {
 				setDisplayTime((prevTime) => prevTime + 1);
 			}
-			else if (displayTime <= 0) {
-				setIsPaused(true);
-				setDisplayTime(0);
-				alert("Time's up!");
-			}
-			setDisplayTime((prevTime) => prevTime - 1);
+			else setDisplayTime((prevTime) => prevTime - 1);
 		}, 1000);
 
 		return () => clearInterval(interval);
 	}
-	, [isPaused, isTimer]);
+	, [isPaused, displayTime]); // Dependencias necesarias aseguran que el efecto se ejecuta
+	// Ambas dependencias cambian durante la ejecución del efecto y son usadas en los condicionales
 	return (
 	<div>
 		<div className="display">{displayTime.toString().padStart(8, "⏱️000000")}</div>
+		<div className="container">
+			<button
+			onClick={() => {
+				setIsPaused(!isPaused); // Alterna entre inicio y pausa
+			}}
+			className={`button ${isPaused ? "start" : "pause"}`}>{isPaused ? "Start" : "Pause"}</button>
+			<button
+			onClick={() => {
+				setIsPaused(true);
+				setDisplayTime(isTimer ? toTimer : 0); // Regresa a los valores iniciales o al valor introducido por el usuario
+			}}
+			className="button">Reset</button>
+		</div>
+		<div className="container">
+			<button // Activa el temporizador
+			onClick={() => {
+				if (isTimer) return;
+				setIsTimer(true);
+				setIsPaused(true);
+				setDisplayTime(toTimer);
+			}}
+			className={`button ${isTimer ? "active" : ""}`}
+			>Timer</button>
+			<button //Activa el cronómetro
+			onClick={() => {
+				if (!isTimer) return;
+				setIsTimer(false);
+				setIsPaused(true);
+				setDisplayTime(0);
+			}}
+			className={`button ${!isTimer ? "active" : ""}`}
+			>Chrono</button>
+		</div>
+		{isTimer?
 		<div className="container">
 		<input
     		type="number"
@@ -42,39 +77,7 @@ const Home = () => {
 			setDisplayTime(Number(e.target.value));
         	}}}
     	/> <p>Type a new value to timer and press Enter</p>
-		</div>
-		<div className="container">
-			<button 
-			onClick={() => {
-				if (isTimer) return;
-				setIsTimer(true);
-				setIsPaused(true);
-				setDisplayTime(toTimer);
-			}}
-			className="button"
-			>Timer</button>
-			<button
-			onClick={() => {
-				if (!isTimer) return;
-				setIsTimer(false);
-				setIsPaused(true);
-				setDisplayTime(0);
-			}}
-			className="button">Chrono</button>
-		</div>
-		<div className="container">
-			<button
-			onClick={() => {
-				setIsPaused(!isPaused);
-			}}
-			className="button">{isPaused ? "Start" : "Pause"}</button>
-			<button
-			onClick={() => {
-				setIsPaused(true);
-				setDisplayTime(isTimer ? toTimer : 0);
-			}}
-			className="button">Reset</button>
-		</div>
+		</div> : null}
 	</div>
 	);
 };
